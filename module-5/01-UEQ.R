237dbb8523139c89610799b2d6b0e980d22b711c
fabbiocrux <fabbiocrux.ms@gmail.com>
2023-02-03 02:44:48 +0100

    putting version

--- module-5/01-UEQ.R
+++ module-5/01-UEQ.R
@@ -1,5 +1,5 @@
-# ================ AttrakDiff Script for data analysis ================
-# Create a reproductible Script that enables the analysis of the AttrakDiff Methodology
+# ================ UEQ Script for data analysis ================
+# Create a reproductible Script that enables the analysis of the UEQ Methodology
 
 # CONTENTS
 #   1. Reading the Excel/CSV Data
@@ -8,86 +8,92 @@
 #   4. Introduce the results in Rmarkdown table.
 
 
-# KEY PACKAGES
-## Install those now if you do not have the latest versions. ----
-#install.packages("tidyverse")  # Tidyverse approach for Data science
-#install.packages("readxl")
-
-
 ## Load the packages ----
 library(tidyverse) # Data Science Tools
 library(readxl)  # Read a Excel File
 
 
 #  1. Reading the CSV Data ----
-UEQ <- read_csv('data/2023/UEQ/F.csv')
+UEQ <- read_csv('UEQ.csv')
 
 ## Quantity of participants
 total <- nrow(UEQ)
+
 
 ## Obtaining the names of the columns
 names(UEQ)
 
 
 #  2. Treating CSV Data ----
-## 2.1 Change the Participants ----
-UEQ <- UEQ %>% mutate( Participant = paste(LETTERS[1 : total])) # Approach 1
-UEQ$Participant <- paste(LETTERS[1 : total]) # Approach 2
-
-names(UEQ)
-
 
 ## Pivot_longer() to arrange each variable
-data <-
+Step.1 <-
    UEQ %>% pivot_longer(cols = c(EFF1 : ATT6),
                                names_to = "Variables",
                                values_to = "Answers")
 
 
 ##. 2.2 Changing the scale of the certains answers ----
-### Reading the Excel for the Parameters  
-onglets <- excel_sheets("data/Parameters.xlsx") # Identify the onglets
+### Reading the Excel for the Parameters
+onglets <- excel_sheets("Parameters.xlsx") # Identify the onglets
 
 # Reading all table
-Parameters_UEQ <- 
-   read_excel(path = "data/Parameters.xlsx", sheet = onglets[2] )
+Parameters_UEQ <-
+   read_excel(path = "Parameters.xlsx", sheet = onglets[2] )
 
 
-### Inversing the Scale
-toInvert <- 
+
+## Step 2
+Step.2 <- #<<
+  Step.1 %>% #<<
+  mutate(
+    change_scale = #<<
+      case_when(
+        Answers == 1  ~  -3, #<<
+        Answers == 2  ~  -2,  #<<
+        Answers == 3  ~  -1,  #<<
+        Answers == 4  ~  0,  #<<
+        Answers == 5  ~  1,  #<<
+        Answers == 6  ~  2,  #<<
+        Answers == 7  ~  3,  #<<
+        TRUE ~ 99 #<< ## High number if a problem arrives
+      )
+  )
+
+### Inversing the Scale option 1
+toInvert <-
    c("EFF1", "EFF4",
      "PERS2", "PERS4",
      "DEP3", "DEP4",
      "STIM1", "STIM4",
-     "NOV1",  "NOV2", 
+     "NOV1",  "NOV2",
      "ATT2", "ATT5", "ATT6")
 
+### Option 2
 toInvert <- Parameters_UEQ %>% filter(Inversion == "Yes") %>% pull(Variables)
 
+
 ### Changing the sign of the answers : Approach Explicit
-data <-
-   data %>% mutate(
+Step.3 <-
+  Step.2 %>% mutate(
       Final_value =
          case_when(
-            Variables %in% toInvert ~ as.numeric(Answers)*(-1),
-            TRUE ~ Answers
+            Variables %in% toInvert ~ change_scale*(-1),
+            TRUE ~ change_scale
          )
    )
 
 
-
-
 ## 2.3 Groups of the Dimmensions ----
-data <-
-   data %>% left_join(Parameters_UEQ, by = "Variables")
-
-
+Step.4 <-
+  Step.3 %>%
+  left_join(Parameters_UEQ, by = "Variables")
 
 
 #  3. Graphic I ----
 ## 3.1 Calculating the mean values and the error
 Table_I <-
-   data %>%
+  Step.4 %>%
    group_by(Scale) %>%
    summarise(Moyenne = mean(Final_value),
              Std = sd(Final_value),
@@ -99,7 +105,7 @@ Graph_I <-
    ggplot() +
    aes(x= Scale, y=Moyenne) +
    geom_bar(stat = "identity") +
-   geom_errorbar( aes(x=Scale, 
+   geom_errorbar( aes(x=Scale,
                       ymin = Moyenne - Se,
                       ymax = Moyenne + Se ),
                   width=0.1, colour="orange", alpha=0.9, size=0.5) +
@@ -131,25 +137,25 @@ Graph_I <-
 ## 4.1 Grouping the mean values and the error
 
 Parameters_UEQ$Scale %>% as.factor() %>%  levels()
-   
-data <-
-   data %>% mutate(
+
+Step.5 <-
+  Step.4 %>% mutate(
       Global_scale =
          case_when(
             str_detect(Scale, "Compréhensibilité") ~ "Qualité Pragmatique (QP)",
             str_detect(Scale, "Efficacité") ~ "Qualité Pragmatique (QP)",
             str_detect(Scale, "Contrôlabilité") ~ "Qualité Pragmatique (QP)",
-            
+
             str_detect(Scale, "Originalité") ~ "Qualité Hédonique",
             str_detect(Scale, "Stimulation") ~ "Qualité Hédonique",
-            
+
             str_detect(Scale, "Attraction") ~ "Attraction",
             TRUE ~ "ATTENTION"
          ))
 
 
 Table_II <-
-   data %>%
+  Step.5 %>%
    group_by(Global_scale) %>%
    summarise(Moyenne = mean(Final_value),
              Std = sd(Final_value),
@@ -160,7 +166,7 @@ Graph_II <-
    ggplot() +
    aes(x= Global_scale, y=Moyenne) +
    geom_bar(stat = "identity") +
-   geom_errorbar( aes(x=Global_scale, 
+   geom_errorbar( aes(x=Global_scale,
                       ymin = Moyenne - Se,
                       ymax = Moyenne + Se ),
                   width=0.1, colour="orange", alpha=0.9, size=0.5) +
@@ -187,24 +193,25 @@ Graph_II <-
 #ggsave("Figures/UEQ-2.jpg", width = 10, height = 5, dpi="print" )
 
 
+
 #  5. Graphic III ----
+
 ## Making the graphic per Variable Item
 
-Table_III <- 
-   data %>% 
+Table_III <-
+   Step.5 %>%
    group_by( Variables ) %>%
    summarise( Moyenne = mean(Final_value))
 
-Parameters_UEQ$Scale %>% as.factor() %>% levels()
 
 Graph_III <-
    Table_III %>%
-   ggplot() + 
-   aes(x = Variables, y=Moyenne, group =1) + 
+   ggplot() +
+   aes(x = Variables, y=Moyenne, group =1) +
    geom_line( color="grey" ) +
    geom_point() +
    scale_y_continuous(name="Moyenne", breaks=seq(-3,3,1), limits=c(-3, 3))+
-   # scale_x_discrete(name="Items", 
+   # scale_x_discrete(name="Items",
    #                  labels= c())+
    coord_flip() +
    annotate("rect", xmin=c(1,7,11,15, 19, 23), xmax=c(6,10,14,18, 22, 26),
